dc56a49f9ed760d79fcfd139c47e26a0d3040c1a
Nikolay Gelemerov <nikolay.gelemerov@endava.com>
2023-03-17 14:29:44 +0200

    Update `PaletteOptions` and `Palette` interfaces augmentation

--- src/router/components/Navigation/Navigation.tsx
+++ src/router/components/Navigation/Navigation.tsx
@@ -1,22 +1,37 @@
+import { makeStyles } from 'tss-react/mui';
+
 import { memo } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { Typography } from '@mui/material';
+
 import pages from '@pages';
 
-import styles from './Navigation.scss';
+const useStyles = makeStyles()((theme) => ({
+  NavLink: {
+    color: theme.palette.custom.main,
+    textDecoration: 'none'
+  },
+  NavLinkActive: {
+    color: theme.palette.secondary.main,
+    fontWeight: 'bold'
+  }
+}));
 
 export const Navigation = memo(() => {
+  const { classes } = useStyles();
+
   return (
     <>
       {pages.map(({ name, path }) => (
         <NavLink
           className={({ isActive }) =>
-            isActive ? `${styles.NavLink} ${styles.NavLinkActive}` : styles.NavLink
+            isActive ? `${classes.NavLink} ${classes.NavLinkActive}` : classes.NavLink
           }
           key={path}
           to={path}
         >
-          {name}
+          <Typography variant="body2">{name}</Typography>
         </NavLink>
       ))}
     </>
