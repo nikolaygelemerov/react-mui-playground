0bae9e29e3da9b0537cf613df0c79e2d6d29a3c5
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2023-03-27 16:00:25 +0300

    Share `devDependencies` from the root `package.json` file

--- packages/shared-ui/src/components/Button/Button.tsx
+++ packages/shared-ui/src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 
 export const Button = memo(() => {
-  return <button type="button">dsd Button</button>;
+  return <button type="button">Button</button>;
 });
 
 Button.displayName = 'Button';
