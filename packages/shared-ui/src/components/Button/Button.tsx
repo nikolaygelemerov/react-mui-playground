fc4b3af6a3f1d54a6dee53c78730e9c3dd0cf6a0
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2023-04-04 23:00:40 +0300

    Test `simple-git-hooks`

--- packages/shared-ui/src/components/Button/Button.tsx
+++ packages/shared-ui/src/components/Button/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
   variant: ButtonPropsMUI['variant'];
 }
 
-export const Button = memo<ButtonProps>(({ onClick, variant }) => {
+export const Button = memo<ButtonProps>(({ onClick, text, variant }) => {
   return <ButtonMUI onClick={onClick} variant={variant} />;
 });
 
